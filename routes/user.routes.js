5c5f173f0bf058ffed892b4b760358b03fabbfdd
Rahul Bhalerao <110394226+rahul181b@users.noreply.github.com>
2024-08-10 21:56:57 +0530

    disfo - 1 complete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { registerUser, getAll, getUser } = require('../controllers/user.controllers');
-const { validateRegistration } = require("../middlewares/user.validator")
+const { validateRegistration } = require("../middlewares/user.validator");
+const { verifyAuth } = require('../middlewares/verifyAuth');
 router.post("/register", validateRegistration, registerUser);
-router.get("/all", getAll);
+router.get("/all", verifyAuth, getAll);
 router.get("/:username", getUser);
 
 module.exports = router;
